6e4b100df3acd588f2890345df4565f98ddbd0a0
trentnixon <trentnixon@gmail.com>
2018-10-22 18:09:23 +1100

    Statto

--- server.js
+++ server.js
@@ -196,5 +196,5 @@ if(process.env.NODE_ENV === 'production'){
 
 }
     // Set server ports and Listener
-        const port = 5000
+        const port = process.env.PORT || 5000;
         app.listen(port, ()=>console.log(`Server Started on port ${port}`))
